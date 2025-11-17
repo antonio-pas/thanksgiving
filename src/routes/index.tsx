9726d4410ab7c4a3063810ae14fc272ca500e0bd
antonio-pas <antonio.b.passos18@gmail.com>
2025-11-17 15:49:23 -0800

    Button styles and new font

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { buttonVariants } from '@/ui'
 export const Route = createFileRoute('/')({
   component: Home,
 })
@@ -6,12 +7,13 @@ export const Route = createFileRoute('/')({
 function Home() {
   return (
     <div className="max-w-5xl w-full px-8 mx-auto flex items-center justify-center min-h-screen flex-col gap-4">
-      <h1 className="text-gray-200 text-6xl font-medium">Enter Your Room Code</h1>
+      <h1 className="text-gray-200 text-6xl font-medium"></h1>
       <p className="text-muted-foreground">Enter a room code to get started with appname.dom</p>
       <div className="flex gap-2">
-        <button 
-          className="text-sm h-7.5 rounded-[5px] bg-primary px-2.75"
-        >
+        <button className={buttonVariants()}>
+          Get Started
+        </button>
+        <button className={buttonVariants()}>
           Get Started
         </button>
       </div>
